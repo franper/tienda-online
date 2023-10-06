5465dc17e1716f2c128c6e5c399e836f62b146d9
Israel <israel@MacBook-Pro-de-Israel.local>
2023-10-06 01:27:42 +0100

    fix copy

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="container-fluid text-center">
-	<h3>Desarrollado por <a href="#">Frank Israel Pereira Cedeño y Daniel Martín Alelú</a></h3>
+	<h3>Desarrollado por <a href="#">Frank Israel Pereira Cedeño</a></h3>
 </footer>
